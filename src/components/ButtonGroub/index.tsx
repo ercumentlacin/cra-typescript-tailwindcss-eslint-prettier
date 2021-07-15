64e578ddf8a5023836e208285e66ac98267feb49
Ercüment Laçın <ercument.lacin@akinon.com>
2021-07-15 06:35:32 +0300

    add bindActionCreators method

--- src/components/ButtonGroub/index.tsx
+++ src/components/ButtonGroub/index.tsx
@@ -1,23 +1,22 @@
 import { useDispatch } from 'react-redux';
-import {
-  decrementNumber,
-  incrementNumber,
-  resetNumber,
-} from '../../store/actions/plusMinus';
+import { bindActionCreators } from 'redux';
+import * as plusMinusActionCreators from '../../store/actions/plusMinus';
 import Button from '../Button';
 import styles from './styles.module.css';
 
 const ButtonGroub = () => {
   const dispatch = useDispatch();
+  const AC = bindActionCreators(plusMinusActionCreators, dispatch);
+
   return (
     <div className={styles.buttonGroup}>
-      <Button variant="green" onClick={() => dispatch(incrementNumber(1))}>
+      <Button variant="green" onClick={() => AC.incrementNumber(1)}>
         Increment
       </Button>
-      <Button variant="red" onClick={() => dispatch(decrementNumber())}>
+      <Button variant="red" onClick={() => AC.decrementNumber()}>
         Decrement
       </Button>
-      <Button variant="black" onClick={() => dispatch(resetNumber())}>
+      <Button variant="black" onClick={() => AC.resetNumber()}>
         Reset
       </Button>
     </div>
